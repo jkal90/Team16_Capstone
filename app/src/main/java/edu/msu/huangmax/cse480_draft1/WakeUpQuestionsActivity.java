210bce0153580331fc0493e1d3dcd71f4ce39e55
Max Huang <huangmax@msu.edu>
2021-02-11 01:17:24 -0500

    Added new view for when the wake up button is tapped

---
+++ app/src/main/java/edu/msu/huangmax/cse480_draft1/WakeUpQuestionsActivity.java
@@ -0,0 +1,14 @@
+package edu.msu.huangmax.cse480_draft1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WakeUpQuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wake_up_questions);
+    }
+}
