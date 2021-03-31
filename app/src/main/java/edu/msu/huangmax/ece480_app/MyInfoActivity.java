da1c142afcf75396f7ae89d84c93709ea696b738
danielpinto <pintodan@msu.edu>
2021-03-31 19:16:25 -0400

    Changes-DPinto

--- app/src/main/java/edu/msu/huangmax/ece480_app/MyInfoActivity.java
+++ app/src/main/java/edu/msu/huangmax/ece480_app/MyInfoActivity.java
@@ -1,24 +1,39 @@
 package edu.msu.huangmax.ece480_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyInfoActivity extends AppCompatActivity {
 
+    private String[] responses = new String[4];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myinfo);
+
+
+
         Button submitButton = findViewById(R.id.submitInfoButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                onSubmit();
             }
         });
+    }
 
+    public void onSubmit() {
+        Intent intent = new Intent(this, ThankYouActivity.class);
+        startActivity(intent);
     }
 }
+
